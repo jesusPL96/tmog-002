aa105ac63364f601085488f524b1f7b5e2846fbe
Jesús Peiró López <jpeirolo@mercadona.es>
2024-02-07 16:51:44 +0100

    Solution without tests, some refactor remaining

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,13 +1,81 @@
 package org.tfoc;
 
+import java.util.ArrayList;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public boolean isValid(String s) {
+    public static void main(String[] args) {
+        System.out.println(isValid("({[[()]]})"));
+    }
 
-        return false;
+    public static boolean isValid(String s) {
+
+        var lastCharacters = new ArrayList<Character>();
+
+        if(s.isEmpty() || s.length() > 10_000){
+            return false;
+        }
+
+        for(int i = 0; i<s.length(); i++){
+
+            switch (s.charAt(i)){
+
+                case '(':
+                    lastCharacters.add('(');
+                    break;
+                case ')':
+                    if(!lastCharacters.isEmpty()){
+                        int listSize = lastCharacters.size()-1;
+                        if(lastCharacters.get(listSize)=='('){
+                            lastCharacters.remove(listSize);
+                        } else{
+                            return false;
+                        }
+                    } else{
+                        return false;
+                    }
+                    break;
+                case '{':
+                    lastCharacters.add('{');
+                    break;
+                case '}':
+                    if(!lastCharacters.isEmpty()){
+                        int listSize = lastCharacters.size()-1;
+                        if(lastCharacters.get(listSize)=='{'){
+                            lastCharacters.remove(listSize);
+                        } else{
+                            return false;
+                        }
+                    } else{
+                        return false;
+                    }
+                    break;
+                case '[':
+                    lastCharacters.add('[');
+                    break;
+                case ']':
+                    if(!lastCharacters.isEmpty()){
+                        int listSize = lastCharacters.size()-1;
+                        if(lastCharacters.get(listSize)=='['){
+                            lastCharacters.remove(listSize);
+                        } else{
+                            return false;
+                        }
+                    } else{
+                        return false;
+                    }
+                    break;
+                default:
+                    return false;
+
+            }
+
+        }
+
+        return lastCharacters.isEmpty();
     }
 
 }
