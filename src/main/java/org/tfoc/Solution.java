a0de3a8e4d09b1f9db415e7e45ee305eba92a60e
Jesús Peiró López <jpeirolo@mercadona.es>
2024-02-08 10:13:56 +0100

    Comments

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -9,24 +9,33 @@ public class Solution {
 
     public static boolean isValid(String s) {
 
+        //ArrayList to add and remove different characters while checking the last one added
         var lastCharacters = new ArrayList<Character>();
 
+        //Checking initial validity of the string
         if(s.isEmpty() || s.length() > 10_000){
             return false;
         }
 
+        //Looping through all characters in the string
         for(int i = 0; i<s.length(); i++){
 
+            //Different cases depending on character type
             switch (s.charAt(i)){
 
+                //When we add, no check is needed
                 case '(':
                     lastCharacters.add('(');
                     break;
+
+                //When we remove, checking if s is empty and which character is the last is needed
                 case ')':
                     if(!lastCharacters.isEmpty()){
                         int listSize = lastCharacters.size()-1;
+                        //If last character is the last, then we remove from the list
                         if(lastCharacters.get(listSize)=='('){
                             lastCharacters.remove(listSize);
+                          //If one of the conditions is false, then we return false
                         } else{
                             return false;
                         }
@@ -70,6 +79,7 @@ public class Solution {
 
         }
 
+        //Finally, the list should be empty to be true, else we return false
         return lastCharacters.isEmpty();
     }
 
