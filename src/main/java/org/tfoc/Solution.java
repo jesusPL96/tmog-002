0622fecf2ed3d17a551f15a991b1c9c0048d1a03
Jesús Peiró López <jpeirolo@mercadona.es>
2024-02-12 08:24:23 +0100

    fix: uncomment

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -9,37 +9,37 @@ public class Solution {
 
     public static boolean isValid(String s) {
 
-        //ArrayList to add and remove different characters while checking the last one added
+
         var lastCharacters = new ArrayList<Character>();
 
-        //Checking initial validity of the string
-        if(s.isEmpty() || s.length() > 10_000){
+
+        if (s.isEmpty() || s.length() > 10_000) {
             return false;
         }
 
-        //Looping through all characters in the string
-        for(int i = 0; i<s.length(); i++){
 
-            //Different cases depending on character type
-            switch (s.charAt(i)){
+        for (int i = 0; i < s.length(); i++) {
 
-                //When we add, no check is needed
+
+            switch (s.charAt(i)) {
+
+
                 case '(':
                     lastCharacters.add('(');
                     break;
 
-                //When we remove, checking if s is empty and which character is the last is needed
+
                 case ')':
-                    if(!lastCharacters.isEmpty()){
-                        int listSize = lastCharacters.size()-1;
-                        //If last character is the last, then we remove from the list
-                        if(lastCharacters.get(listSize)=='('){
+                    if (!lastCharacters.isEmpty()) {
+                        int listSize = lastCharacters.size() - 1;
+
+                        if (lastCharacters.get(listSize) == '(') {
                             lastCharacters.remove(listSize);
-                          //If one of the conditions is false, then we return false
-                        } else{
+
+                        } else {
                             return false;
                         }
-                    } else{
+                    } else {
                         return false;
                     }
                     break;
@@ -47,14 +47,14 @@ public class Solution {
                     lastCharacters.add('{');
                     break;
                 case '}':
-                    if(!lastCharacters.isEmpty()){
-                        int listSize = lastCharacters.size()-1;
-                        if(lastCharacters.get(listSize)=='{'){
+                    if (!lastCharacters.isEmpty()) {
+                        int listSize = lastCharacters.size() - 1;
+                        if (lastCharacters.get(listSize) == '{') {
                             lastCharacters.remove(listSize);
-                        } else{
+                        } else {
                             return false;
                         }
-                    } else{
+                    } else {
                         return false;
                     }
                     break;
@@ -62,14 +62,14 @@ public class Solution {
                     lastCharacters.add('[');
                     break;
                 case ']':
-                    if(!lastCharacters.isEmpty()){
-                        int listSize = lastCharacters.size()-1;
-                        if(lastCharacters.get(listSize)=='['){
+                    if (!lastCharacters.isEmpty()) {
+                        int listSize = lastCharacters.size() - 1;
+                        if (lastCharacters.get(listSize) == '[') {
                             lastCharacters.remove(listSize);
-                        } else{
+                        } else {
                             return false;
                         }
-                    } else{
+                    } else {
                         return false;
                     }
                     break;
@@ -79,7 +79,7 @@ public class Solution {
 
         }
 
-        //Finally, the list should be empty to be true, else we return false
+
         return lastCharacters.isEmpty();
     }
 
